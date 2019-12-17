e9f82f991eb63d7ad3255e46e9407a5b84bb072a
Newester <761249549@qq.com>
2019-12-17 21:18:04 +0800

    ...

---
+++ go-grpc/go.mod
@@ -0,0 +1,11 @@
+module github.com/Newester/tdc_pre/go-grpc
+
+go 1.12
+
+require (
+	github.com/golang/glog v0.0.0-20160126235308-23def4e6c14b
+	github.com/golang/protobuf v1.3.2
+	github.com/grpc-ecosystem/grpc-gateway v1.12.1
+	golang.org/x/net v0.0.0-20191002035440-2ec189313ef0
+	google.golang.org/grpc v1.24.0
+)
---
+++ go-grpc/http_server.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"flag"
+	"net/http"
+	"context"
+)
+
+var (
+	echoEndpoint = flag.String("echo_endpoint", "localhost:50051", "endpoint of YourService")
+)
+
+func run() error {
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	mux := runtime.NewServeMux()
+	opts := []grpc.DialOption{grpc.WithInsecure()}
+	err := gw.RegisterGreeterHandlerFromEndpoint(ctx, mux, *echoEndpoint, opts)
+	if err != nil {
+		return err
+	}
+
+	return http.ListenAndServe(":8080", mux)
+}
+
+func main() {
+	flag.Parse()
+	defer glog.Flush()
+
+	if err := run(); err != nil {
+		glog.Fatal(err)
+	}
+}
